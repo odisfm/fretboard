915cba362d795b11f0f7147c28ed0a4eee8c87db
odisfm <102568741+odisfm@users.noreply.github.com>
2026-09-12 11:15:06 +1000

    feat: add Button

--- client/src/components/TuningDemo/StringSetter.tsx
+++ client/src/components/TuningDemo/StringSetter.tsx
@@ -1,5 +1,6 @@
 import {FaMinusCircle, FaPlusCircle, FaTrash} from "react-icons/fa"
 import {midiPitchToNoteName} from "@fretboard/shared/src/utils/midiPitchToNoteName.ts";
+import Button from "../generic/Button.tsx";
 
 type Props = {
     pitch: number;
@@ -11,31 +12,32 @@ type Props = {
 export default function StringSetter({pitch, idx, incrementPitch, deleteString}: Props) {
     return (
         <div className={`flex gap-2 w-full`}>
-            <div className={`flex gap-1`}>
-                <button
-                    className={``}
+            <div className={`flex`}>
+                <Button
+                    variant={"subtle"}
                     onClick={() => incrementPitch(idx, -1)}
                 >
                     <FaMinusCircle/>
-                </button>
-                <button
-                    className={``}
+                </Button>
+                <Button
+                    variant={"subtle"}
                     onClick={() => incrementPitch(idx, 1)}
                 >
                     <FaPlusCircle/>
-                </button>
+                </Button>
             </div>
             <div
                 className={`font-bold ml-6`}
             >
                 <span>{midiPitchToNoteName(pitch, true)}</span>
             </div>
-            <button
+            <Button
                 onClick={() => deleteString(idx)}
-                className={`ml-auto`}
+                variant={"warning"}
+                styles={`ml-auto`}
             >
                 <FaTrash/>
-            </button>
+            </Button>
         </div>
     )
 }
---
+++ client/src/components/generic/Button.tsx
@@ -0,0 +1,61 @@
+import Spinner from "./Spinner.tsx";
+
+type Props = {
+    disabled?: boolean
+    loading?: boolean
+    variant?: "default" | "warning" | "subtle" | "unstyled"
+    onClick?: (event: React.MouseEvent<HTMLElement>) => void;
+    children?: React.ReactNode;
+    styles?: string
+}
+
+export default function Button(
+    {
+        disabled,
+        loading,
+        onClick,
+        children,
+        variant = "default",
+        styles,
+
+
+    }: Props) {
+    let variantStyles: string
+    switch (variant) {
+        case "default":
+            variantStyles = "bg-slate-600 hover:bg-slate-700";
+            break;
+        case "warning":
+            variantStyles = "bg-red-700 hover:bg-red-600";
+            break;
+        case "subtle":
+            variantStyles = "border-2 border-transparent hover:border-white/10 hover:bg-white/5";
+            break;
+        default:
+            variantStyles = "";
+            break;
+    }
+    let cursorStyle = "cursor-pointer";
+    if (disabled) {
+        cursorStyle = "cursor-not-allowed";
+    } else if (loading) {
+        cursorStyle = "cursor-wait";
+    }
+
+    return (
+        <button
+            onClick={(e) => {
+                if (disabled || loading || !onClick) return
+                onClick(e)
+            }}
+            disabled={disabled}
+            className={`
+            flex items-center gap-2 py-1 px-2 rounded-sm
+            ${cursorStyle} ${styles} ${variantStyles} 
+            `}
+        >
+            {children}
+            {loading ? <Spinner /> : null}
+        </button>
+    )
+}
