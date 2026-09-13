761f48be85acb3c2cb7ec5d599116160c7f0cb7a
odisfm <102568741+odisfm@users.noreply.github.com>
2026-09-13 23:39:20 +1000

    feat: add BinaryToggle

---
+++ client/src/components/generic/BinaryToggle.tsx
@@ -0,0 +1,25 @@
+import Button from "./Button.tsx";
+import { TiTick } from "react-icons/ti";
+
+type Props = {
+    state: boolean,
+    fn: (newState: boolean) => void,
+}
+
+export function BinaryToggle({state, fn}: Props) {
+    const onStyles = `bg-white text-black hover:bg-neutral-300`
+    const offStyles = `bg-neutral-700 hover:bg-neutral-600 text-white`
+    return (
+        <div className={`mt-1.5 flex rounded-md w-15 h-5 bg-black ${state ? "justify-end" : "justify-start"}`}>
+            <Button
+                onClick={() => fn(!state)}
+                variant="unstyled"
+                styles={`w-8 ${state ? onStyles : offStyles}`}
+            >
+                {
+                    state && <TiTick/>
+                }
+            </Button>
+        </div>
+    )
+}
