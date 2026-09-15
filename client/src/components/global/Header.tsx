4dc0cc792a0ab7b508866987d1c15eda55fda0be
odisfm <102568741+odisfm@users.noreply.github.com>
2026-09-16 09:50:36 +1000

    feat: make logo link to /

--- client/src/components/global/Header.tsx
+++ client/src/components/global/Header.tsx
@@ -1,14 +1,16 @@
 import {NetworkStatus} from "../NetworkStatus.tsx";
 import Button from "../generic/Button.tsx";
 import {useAuth} from "../../contexts/auth/useAuth.ts";
-import {useNavigate} from "react-router";
+import {Link, useNavigate} from "react-router";
 
 export function Header() {
     const authContext = useAuth()
     const navigate = useNavigate()
     return (
         <header className={`bg-black p-2 flex gap-2`}>
-            <span className={`font-bold text-lg mr-auto`}>fretboard</span>
+            <Link to={"/"} className={`mr-auto py-1 px-2 rounded-md hover:bg-neutral-800`}>
+                <h1 className={`font-bold text-lg`}>fretboard</h1>
+            </Link>
             <NetworkStatus/>
             { authContext.auth ?
                 <Button onClick={authContext.logOut}>
