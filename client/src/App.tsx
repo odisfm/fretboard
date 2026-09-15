b8cda3a822bdd92293d763753d4088cd05b7d4a7
odisfm <102568741+odisfm@users.noreply.github.com>
2026-09-16 09:47:01 +1000

    refactor: break out Header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,14 @@
 import {UserDataProvider} from "./contexts/userData/UserDataProvider.tsx";
 import {AudioProvider} from "./contexts/audio/AudioProvider.tsx";
-import {NetworkStatus} from "./components/NetworkStatus.tsx";
-import {Outlet, useNavigate} from "react-router";
-import {useAuth} from "./contexts/auth/useAuth.ts";
-import Button from "./components/generic/Button.tsx";
+import {Outlet} from "react-router";
+import {Header} from "./components/global/Header.tsx";
 
 export function App() {
-    const authContext = useAuth()
-    const navigate = useNavigate()
 
     return (
             <UserDataProvider>
                 <div className={`w-[100vw]`}>
-                    <header className={`bg-black p-2 flex gap-2`}>
-                        <span className={`font-bold text-lg mr-auto`}>fretboard</span>
-                        <NetworkStatus/>
-                        { authContext.auth ?
-                            <Button onClick={authContext.logOut}>
-                            log out
-                            </Button>
-                            :
-                            <Button onClick={() => navigate("/login")}>
-                                log in
-                            </Button>
-                        }
-                    </header>
+                    <Header />
                     <AudioProvider>
                         <div className={`p-4`}>
                             <Outlet/>
