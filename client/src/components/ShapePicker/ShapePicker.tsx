f5f14fcef046fe5d6ea3ff137cc30a657a0ba241
odisfm <102568741+odisfm@users.noreply.github.com>
2026-09-20 09:04:32 +1000

    ui: style

--- client/src/components/ShapePicker/ShapePicker.tsx
+++ client/src/components/ShapePicker/ShapePicker.tsx
@@ -2,6 +2,7 @@ import { Virtuoso } from 'react-virtuoso'
 import Button from "../generic/Button.tsx";
 import ShapeButton from "./ShapeButton.tsx";
 import type {FingerShape} from "@fretboard/shared/types/fingerShape";
+import {FaPalette} from "react-icons/fa";
 
 type Props = {
     fingerShapes: FingerShape[],
@@ -19,8 +20,10 @@ export default function ShapePicker({fingerShapes, onClick, active, setScrollToF
                         onClick(null)
                     }}
                     variant={active === null ? "default" : "subtle"}
+                    styles={`w-20 flex flex-col items-center justify-center p-4 border-2! border-white/10! `}
                 >
-                    {"Whole fretboard"}
+                    <FaPalette/>
+                    <span>{"Whole fretboard"}</span>
                 </Button>
                 {fingerShapes.length === 0 &&
                     <span className={`self-center w-xs text-center p-4 rounded-md bg-red-950`}>
