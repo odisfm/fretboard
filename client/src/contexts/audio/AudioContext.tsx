a0147e451871ec175c938e495be4c60f45afb5f0
odisfm <102568741+odisfm@users.noreply.github.com>
2026-09-21 23:22:27 +1000

    feat: add audio previews

--- client/src/contexts/audio/AudioContext.tsx
+++ client/src/contexts/audio/AudioContext.tsx
@@ -2,6 +2,8 @@ import {createContext} from "react";
 
 type AudioContextValue = {
     playNote: (pitch: string, length?: string) => void;
+    playNotes: (pitches: string[], length?: string, space?: number, chord?: boolean) => void;
+    releaseAll: () => void;
 }
 
 export const AudioContext = createContext<AudioContextValue | null>(null)
