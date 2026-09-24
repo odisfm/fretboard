4c9b02e3dc8de878fe31f31929448309146bb9fb
odisfm <102568741+odisfm@users.noreply.github.com>
2026-09-24 18:11:26 +1000

    fix: chord name search always sets to current root

--- client/src/components/ChordPicker/ChordPicker.tsx
+++ client/src/components/ChordPicker/ChordPicker.tsx
@@ -101,9 +101,9 @@ export function ChordPicker({chordPickerOptions, setChordPickerOptions}: {
             return;
         }
         setBadChordNameSearch(false)
-        chordContext.setChord(chord)
         const options = getChordPickerOptionsFromChord(chord)
         setChordPickerOptions(options)
+        chordContext.setChord(chord)
     }
 
 
